0ee279d7e4f4af42d3c640b31f76655a2cf45675
juppcamper <juppcamper@googlemail.com>
2023-01-22 20:11:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,38 +5,38 @@ import pandas as pd
 import os
 os.system("playwright install")
 
-allnames = ["raubbau"]
+names = st.text_input()
 
 
 def UserNameInfo():
-    for names in allnames:
-        with TikTokAPI() as api:
-            user = api.user(names)
-            print(len(user.videos._light_models))
-            for video in user.videos:
-                account = video.author
-                id = video.id
-                time = video.create_time.strftime("%d.%m.%Y, %H:%M:%S")
-                
-                cover = video.video.cover
-                dynamic_cover = video.video.dynamic_cover
-                
-                url = video.video.play_addr
 
-                likes = video.stats.digg_count
-                views = video.stats.play_count
-                shares = video.stats.share_count
-                comments = video.stats.comment_count
+    with TikTokAPI() as api:
+        user = api.user(names)
+        print(len(user.videos._light_models))
+        for video in user.videos:
+            account = video.author
+            id = video.id
+            time = video.create_time.strftime("%d.%m.%Y, %H:%M:%S")
                 
-                length = video.video.duration
-                description = video.desc
+            cover = video.video.cover
+            dynamic_cover = video.video.dynamic_cover
                 
-                sound_name = video.music.title
-                sound_url = video.music.play_url
+            url = video.video.play_addr
+
+            likes = video.stats.digg_count
+            views = video.stats.play_count
+            shares = video.stats.share_count
+            comments = video.stats.comment_count
+                
+            length = video.video.duration
+            description = video.desc
+                
+            sound_name = video.music.title
+            sound_url = video.music.play_url
                 # sound_author = video.music.author_name
 
-                df = pd.DataFrame([[account,id,cover,dynamic_cover,url,length,time,likes,views,shares,comments,description,sound_name,sound_url]])
-                st.dataframe(df) 
+            df = pd.DataFrame([[account,id,cover,dynamic_cover,url,length,time,likes,views,shares,comments,description,sound_name,sound_url]])
+            st.dataframe(df) 
                 # df.to_csv('{}_videos.csv'.format(account), mode='a', index=False, header=False)
 
     
