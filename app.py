cc11257c1eef90d839ce229437a65ebd830c56a1
juppcamper <juppcamper@googlemail.com>
2023-01-23 00:07:27 +0100

    Update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ os.system("playwright install")
 
 names = st.text_input("Account")
 
-st.write("alpha_0.0.2")
+st.write("alpha_0.0.3")
 
 df = pd.DataFrame(columns = ["account", "id", "cover", "dynamic_cover", "url", "length", "time", "likes", "views", "shares", "comments", "description", "sound_name", "sound_url"])
 
@@ -38,11 +38,11 @@ def UserNameInfo():
             sound_url = video.music.play_url
                 # sound_author = video.music.author_name
 
-            df = pd.concat([[account,id,cover,dynamic_cover,url,length,time,likes,views,shares,comments,description,sound_name,sound_url]], ignore_index = True)
-             
+            df = pd.concat([df, pd.DataFrame([[account,id,cover,dynamic_cover,url,length,time,likes,views,shares,comments,description,sound_name,sound_url]], columns = df.columns)], ignore_index=True)
+        st.dataframe(df)     
             # df.to_csv('{}_videos.csv'.format(account), mode='a', index=False, header=False)
         
-        st.dataframe(df)
+        
 
     
 UserNameInfo()
