99c1625369db22ffd7e25ac3c305a26f18f2640e
juppcamper <juppcamper@googlemail.com>
2023-01-22 16:54:01 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from tiktokapipy.api import TikTokAPI
+import pandas as pd
+
+
+allnames = ["raubbau"]
+
+
+def UserNameInfo():
+    for names in allnames:
+        with TikTokAPI(scroll_down_time = 30) as api:
+            user = api.user(names)
+            print(len(user.videos._light_models))
+            for video in user.videos:
+                account = video.author
+                id = video.id
+                time = video.create_time.strftime("%d.%m.%Y, %H:%M:%S")
+                
+                cover = video.video.cover
+                dynamic_cover = video.video.dynamic_cover
+                
+                url = video.video.play_addr
+
+                likes = video.stats.digg_count
+                views = video.stats.play_count
+                shares = video.stats.share_count
+                comments = video.stats.comment_count
+                
+                length = video.video.duration
+                description = video.desc
+                
+                sound_name = video.music.title
+                sound_url = video.music.play_url
+                # sound_author = video.music.author_name
+
+                df = pd.DataFrame([[account,id,cover,dynamic_cover,url,length,time,likes,views,shares,comments,description,sound_name,sound_url]])
+                st.dataframe(df) 
+                # df.to_csv('{}_videos.csv'.format(account), mode='a', index=False, header=False)
+
+    
+UserNameInfo()
